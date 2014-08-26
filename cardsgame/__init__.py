3b9f30bf968c3de6896f107307ccde11387faf75
Harry Reeder <harry@harryreeder.co.uk>
2014-08-26 12:40:17 +0100

    Begin Join Game, Use two outputs in open game.

--- cardsgame/__init__.py
+++ cardsgame/__init__.py
@@ -9,6 +9,7 @@ class CardsGame(object):
         
     def register_commands(self, xmpp):
         xmpp.register_command("open", self.open_game)
+        xmpp.register_command("join", self.join_game)
         
     def get_jid(self, msg):
         muc = msg['from'].bare
@@ -27,5 +28,23 @@ class CardsGame(object):
 
         self.game_open=True
         self.players.append(self.get_jid(msg))
-
-        return "%s has opened a game of Cards Against Humanity! Use %sjoin to join the game!\r\n%s may use %sexpansions to configure which expansions will be used in play." % (msg['from'].resource, self.client.config.xmpp['trigger'], msg['from'].resource, self.client.config.xmpp['trigger'])
+        
+        self.client.send_message(mto=msg['from'].bare,
+                                 mtype="groupchat",
+                                 mbody="%s has opened a game of Cards Against Humanity! Use %sjoin to join the game!" % (msg['from'].resource, self.client.config.xmpp['trigger'])
+                                 )
+        
+        self.client.send_message(mto=msg['from'].bare,
+                                 mtype="groupchat",
+                                 mbody="%s may use %sexpansions to configure which expansions will be used in play." % (msg['from'].resource, self.client.config.xmpp['trigger'])
+                                 )
+        
+        return
+    
+    def join_game(self, msg):
+        if self.game_in_progress:
+            return "The game has already started, and I am unable to join you into the game right now"
+        if not self.game_open:
+            return "There is no game open to join. Open one with %sopen" % (self.client.config.xmpp['trigger'])
+        
+        
